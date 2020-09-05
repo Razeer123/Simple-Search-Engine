21fdda7fc71cc8ba8aa33c839ccfe1dbb69ff54d
Michał <razeer123@interia.pl>
2020-09-05 21:06:13 +0200

    Updated search engine to handle many inputs

--- src/com/derejmichal/Main.java
+++ src/com/derejmichal/Main.java
@@ -1,17 +1,50 @@
 package com.derejmichal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String inputLine = scanner.nextLine();
-        String word = scanner.nextLine();
+        StringEngine engine = new StringEngine();
+        System.out.println("Enter the number of people:");
+        int inputNumber = scanner.nextInt();
+        List<String> inputList = new ArrayList<>();
 
-        StringEngine engine = new StringEngine(inputLine, word);
-        engine.findWordIndex();
+        System.out.println("Enter all people:");
+        scanner.nextLine();
+        for (int i = 0; i < inputNumber; i++) {
+            inputList.add(scanner.nextLine());
+        }
 
+        System.out.println();
+        System.out.println("Enter the number of search queries:");
+        int numberQueries = scanner.nextInt();
+        String query;
+        scanner.nextLine();
+
+        for (int i = 0; i < numberQueries; i++) {
+
+            System.out.println();
+            System.out.println("Enter data to search people:");
+            query = scanner.nextLine();
+            List<Integer> result = engine.findIndexesArray(inputList, query);
+
+            if (result.isEmpty()) {
+                System.out.println("No matching people found.");
+            } else {
+                System.out.println();
+                System.out.println("Found people:");
+                for (Integer integer : result) {
+                    System.out.println(inputList.get(integer));
+                }
+            }
+
+            result.clear();
+
+        }
     }
 }
 
--- src/com/derejmichal/StringEngine.java
+++ src/com/derejmichal/StringEngine.java
@@ -1,30 +1,34 @@
 package com.derejmichal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringEngine {
 
-    private final String inputLine;
-    private final String toSearch;
-
-    public StringEngine(String inputLine, String toSearch) {
-        this.inputLine = inputLine;
-        this.toSearch = toSearch;
-    }
-
-    public void findWordIndex() {
+    public boolean findWord(String inputLine, String toSearch) {
 
         String[] wordList =  inputLine.split(" ");
         int index = 0;
 
         for (int i = 0; i < wordList.length; i++) {
-            if (wordList[i].equals(toSearch)) {
+            if (wordList[i].toLowerCase().replace(" ", "").contains(toSearch.toLowerCase())) {
                 index = i + 1;
             }
         }
 
-        if (index == 0) {
-            System.out.println("Not Found");
-        } else {
-            System.out.println(index);
+        return index != 0;
+    }
+
+    public List<Integer> findIndexesArray(List<String> inputList, String query) {
+
+        List<Integer> intList = new ArrayList<>();
+
+        for (int i = 0; i < inputList.size(); i++) {
+            if (findWord(inputList.get(i), query)) {
+                intList.add(i);
+            }
         }
+
+        return intList;
     }
 }
